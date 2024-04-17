6219628e60896ff17f3e09deca86579e7954520f
Giovana Miyuki <miyukigiovana@gmail.com>
2024-04-17 01:19:41 -0300

    feat(p1-pdmt): cria componente principal e faz hello

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSquareCheck, faPenClip, faTrashCan
+} from '@fortawesome/free-solid-svg-icons'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+
+    <br /><br />
+    <h2> ✨ Hello, Lembretes! ✨ </h2>
+    <br /><br />
+
     </div>
   );
 }
